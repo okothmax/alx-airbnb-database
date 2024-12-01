1af6fa909bfe4eeaf107b57f3ebcc0091a64336c
okothmax <okothmaxwell03@gmail.com>
2024-12-01 11:48:42 +0300

    ordered the query

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -25,7 +25,9 @@ SELECT
 FROM 
     Property
 LEFT JOIN 
-    Review ON Property.property_id = Review.property_id;
+    Review ON Property.property_id = Review.property_id
+ORDER BY 
+    Property.pricepernight ASC;
 
 
 -- FULL OUTER JOIN: Retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user.
