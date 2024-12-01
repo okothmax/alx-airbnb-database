a2bd14482661d2718de1c10602132f7035961a67
okothmax <okothmaxwell03@gmail.com>
2024-12-01 12:15:25 +0300

    optimization

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,4 +1,38 @@
--- Initial Query: Retrieve all bookings with user, property, and payment details
+-- Refactored Query: Retrieve all bookings with user, property, and payment details and additional filters
+SELECT 
+    B.booking_id,
+    B.start_date,
+    B.end_date,
+    B.total_price,
+    U.user_id,
+    U.first_name,
+    U.last_name,
+    U.email,
+    P.property_id,
+    P.name AS property_name,
+    P.location AS property_location,
+    P.pricepernight,
+    Pay.payment_id,
+    Pay.amount AS payment_amount,
+    Pay.payment_date,
+    Pay.payment_method
+FROM 
+    Booking B
+INNER JOIN 
+    User U ON B.user_id = U.user_id
+INNER JOIN 
+    Property P ON B.property_id = P.property_id
+INNER JOIN 
+    Payment Pay ON B.booking_id = Pay.booking_id
+WHERE 
+    B.total_price > 100  
+    AND B.start_date >= CURRENT_DATE  
+    AND Pay.payment_method = 'paypal'  
+ORDER BY 
+    B.start_date; 
+
+
+EXPLAIN ANALYZE
 SELECT 
     B.booking_id,
     B.start_date,
